77834b7cc60b45fe9406293d9d163c71112625b4
shj6214 <shj_1994@naver.com>
2021-07-15 11:48:06 +0900

    2nd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like shjoo!');
+    res.send('I like seunghwanjoo!');
 });
 
 app.get('/start', function(req, res){
